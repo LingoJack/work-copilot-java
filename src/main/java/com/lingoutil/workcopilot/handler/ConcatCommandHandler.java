4294c4689ea5317aeef68334b6a190ca0d438c32
1-6 <a1-6@1-6s-MacBook-Air.local>
2024-12-04 20:33:17 +0800

    重构了部分代码使之concat和运行等命令兼容MAC

--- src/main/java/com/lingoutil/workcopilot/handler/ConcatCommandHandler.java
+++ src/main/java/com/lingoutil/workcopilot/handler/ConcatCommandHandler.java
@@ -1,6 +1,7 @@
 package com.lingoutil.workcopilot.handler;
 
 import com.lingoutil.workcopilot.config.YamlConfig;
+import com.lingoutil.workcopilot.runner.CommandRunner;
 import com.lingoutil.workcopilot.util.LogUtil;
 
 import java.io.File;
@@ -11,54 +12,67 @@ import java.util.List;
 import static com.lingoutil.workcopilot.constant.Constant.*;
 
 public class ConcatCommandHandler extends CommandHandler {
-    @Override
-    protected List<String> loadCommandList() {
-        return concatCommands;
-    }
+	@Override
+	protected List<String> loadCommandList() {
+		return concatCommands;
+	}
 
-    @Override
-    protected void process(String[] argv) {
-        String scriptName = argv[2];
-        String scriptContent = argv[3];
-        String deportFolderPath = YamlConfig.getProperty(SCRIPT, DEPOT);
+	@Override
+	protected void process(String[] argv) {
+		String scriptName = argv[2];
+		String scriptContent = argv[3];
+		String deportFolderPath = YamlConfig.getProperty(SCRIPT, DEPOT);
 
-        // 检查脚本名是否已存在
-        if (YamlConfig.containProperty(PATH, scriptName)) {
-            LogUtil.error("❌ 失败！脚本名 {%s} 已经存在", scriptName);
-            return;
-        }
+		// 检查脚本名是否已存在
+		if (YamlConfig.containProperty(PATH, scriptName)) {
+			LogUtil.error("❌ 失败！脚本名 {%s} 已经存在", scriptName);
+			return;
+		}
 
-        String scriptPath = deportFolderPath + scriptName + ".cmd";
-        YamlConfig.addNestedProperty(PATH, scriptName, scriptPath);
+		// 生成脚本路径
+		String scriptPath = deportFolderPath + scriptName;
+		scriptPath += CommandRunner.getOsType().equals(CommandRunner.WINDOWS) ? ".cmd" : ".sh";
+		YamlConfig.addNestedProperty(PATH, scriptName, scriptPath);
 
-        // 写入文件
-        try {
-            File scriptFile = new File(scriptPath);
-            if (!scriptFile.exists()) {
-                scriptFile.getParentFile().mkdirs(); // 确保父目录存在
-                scriptFile.createNewFile(); // 创建文件
-                LogUtil.info("🎉 文件创建成功: %s", scriptPath);
-            }
+		try {
+			// 写入文件
+			File scriptFile = new File(scriptPath);
+			if (!scriptFile.exists()) {
+				scriptFile.getParentFile().mkdirs(); // 确保父目录存在
+				scriptFile.createNewFile(); // 创建文件
+				LogUtil.info("🎉 文件创建成功: %s", scriptPath);
+			}
 
-            try (FileWriter writer = new FileWriter(scriptFile)) {
-                writer.write(scriptContent.substring(1, scriptContent.length() - 1));
-                YamlConfig.addNestedProperty(SCRIPT, scriptName, scriptPath);
-                LogUtil.info("✅ 成功创建脚本 {%s} 并写入内容: %s", scriptName, scriptContent);
-            }
-        }
-        catch (IOException e) {
-            YamlConfig.removeNestedProperty(PATH, scriptName);
-            LogUtil.error("💥 写入脚本文件失败: %s", e.getMessage());
-        }
-    }
+			try (FileWriter writer = new FileWriter(scriptFile)) {
+				writer.write(scriptContent.substring(1, scriptContent.length() - 1));
+				LogUtil.info("✅ 成功创建脚本 {%s} 并写入内容: %s", scriptName, scriptContent);
+			}
 
-    @Override
-    protected boolean checkArgs(String[] argv) {
-        return checkArgs(argv, 4, this::hint);
-    }
+			// 设置执行权限 (非 Windows 系统)
+			if (!CommandRunner.getOsType().equals(CommandRunner.WINDOWS)) {
+				if (!scriptFile.setExecutable(true)) {
+					throw new IOException("无法设置脚本的执行权限");
+				}
+				LogUtil.info("🔧 已为脚本 {%s} 设置执行权限", scriptName);
+			}
 
-    @Override
-    protected void hint(String[] argv) {
-        LogUtil.usage("%s %s <script_name> \"<script_content>\"", argv[0], argv[1]);
-    }
+			YamlConfig.addNestedProperty(SCRIPT, scriptName, scriptPath);
+
+		} catch (IOException e) {
+			// 失败时清理
+			YamlConfig.removeNestedProperty(PATH, scriptName);
+			LogUtil.error("💥 处理脚本文件失败: %s", e.getMessage());
+		}
+	}
+
+
+	@Override
+	protected boolean checkArgs(String[] argv) {
+		return checkArgs(argv, 4, this::hint);
+	}
+
+	@Override
+	protected void hint(String[] argv) {
+		LogUtil.usage("%s %s <script_name> \"<script_content>\"", argv[0], argv[1]);
+	}
 }
--- src/main/java/com/lingoutil/workcopilot/runner/CommandRunner.java
+++ src/main/java/com/lingoutil/workcopilot/runner/CommandRunner.java
@@ -4,7 +4,9 @@ import com.lingoutil.workcopilot.config.YamlConfig;
 import com.lingoutil.workcopilot.util.LogUtil;
 import com.lingoutil.workcopilot.util.URLUtil;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 
@@ -12,216 +14,271 @@ import static com.lingoutil.workcopilot.constant.Constant.*;
 
 public class CommandRunner {
 
-    private static final StringBuilder sb = new StringBuilder();
+	private static final StringBuilder sb = new StringBuilder();
 
-    // 静态变量存储操作系统类型
-    private static final String osType;
+	// 静态变量存储操作系统类型
+	private static final String osType;
 
-    public static final String WINDOWS = "win";
+	public static final String WINDOWS = "win";
 
-    public static final String MAC = "mac";
+	public static final String MAC = "mac";
 
-    // 静态代码块在类加载时判断操作系统类型
-    static {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains(WINDOWS)) {
-            osType = WINDOWS;
-        }
-        else if (os.contains("mac")) {
-            osType = MAC;
-        }
-        else {
-            osType = "unknown";
-        }
-    }
+	// 静态代码块在类加载时判断操作系统类型
+	static {
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.contains(WINDOWS)) {
+			osType = WINDOWS;
+		}
+		else if (os.contains("mac")) {
+			osType = MAC;
+		}
+		else {
+			osType = "unknown";
+		}
+	}
 
-    public static void run(String[] argv) {
-        String script = argv[0];
-        String alias = argv[1];
-        int length = argv.length;
+	public static void run(String[] argv) {
+		String script = argv[0];
+		String alias = argv[1];
+		int length = argv.length;
 
-        // 检查别名是否存在
-        if (!isAliasExist(alias)) {
-            LogUtil.error("❌ 无法找到别名对应的路径或网址 {%s}。请检查配置文件。", alias);
-            return;
-        }
+		// 检查别名是否存在
+		if (!isAliasExist(alias)) {
+			LogUtil.error("❌ 无法找到别名对应的路径或网址 {%s}。请检查配置文件。", alias);
+			return;
+		}
 
-        if (YamlConfig.containProperty(BROWSER, alias)) {
-            openBrowser(argv, length, alias, script);
-        }
-        else if (YamlConfig.containProperty(EDITOR, alias)) {
-            openEditor(argv, length, alias);
-        }
-        else if (YamlConfig.containProperty(VPN, alias)) {
-            openVPN(alias);
-        }
-        else {
-            // path是应用路径，如何打开
-            open(alias);
-        }
-    }
+		if (YamlConfig.containProperty(BROWSER, alias)) {
+			openBrowser(argv, length, alias, script);
+		}
+		else if (YamlConfig.containProperty(EDITOR, alias)) {
+			openEditor(argv, length, alias);
+		}
+		else if (YamlConfig.containProperty(VPN, alias)) {
+			openVPN(alias);
+		}
+		else if (YamlConfig.containProperty(SCRIPT, alias)) {
+			runScript(alias);
+		}
+		else {
+			// path是应用路径，如何打开
+			open(alias);
+		}
+	}
 
-    private static void openVPN(String alias) {
-        open(alias);
-    }
+	private static void runScript(String alias) {
+		String path = YamlConfig.getProperty(SCRIPT, alias);
 
-    private static void openEditor(String[] argv, int length, String alias) {
-        if (length != 3) {
-            open(alias);
-        }
-        else {
-            String filePath = argv[2];
-            open(alias, filePath);
-        }
-    }
+		// 打印调试信息
+		LogUtil.info("⚙️ 即将执行脚本，路径: %s", path);
 
-    private static void openBrowser(String[] argv, int length, String alias, String script) {
-        if (length == 2) {
-            open(alias);
-        }
-        else {
-            openBrowserWithUrlAliasOrSearchedContent(argv, length, script, alias);
-        }
-    }
+		try {
+			String[] command;
 
-    private static boolean isAliasExist(String alias) {
-        return YamlConfig.containProperty(PATH, alias)
-                || YamlConfig.containProperty(INNER_URL, alias)
-                || YamlConfig.containProperty(OUTER_URL, alias);
-    }
+			if (MAC.equals(osType)) {
+				// 在 macOS 上新开一个终端窗口运行脚本
+				command = new String[]{"open", "-a", "Terminal", path}; // 替换 Terminal 为 iTerm 可改用 iTerm
+			}
+			else if (WINDOWS.equals(osType)) {
+				// Windows 环境运行脚本，添加是否需要新窗口的逻辑
+				boolean openNewWindow = true; // 根据需求设置此值
+				if (openNewWindow) {
+					// 新窗口运行
+					command = new String[]{"cmd.exe", "/c", "start", "cmd.exe", "/k", path};
+				}
+				else {
+					// 当前窗口运行
+					command = new String[]{"cmd.exe", "/c", path};
+				}
+			}
+			else {
+				LogUtil.error("❌ 不支持的操作系统: %s", osType);
+				return;
+			}
 
-    private static void openBrowserWithUrlAliasOrSearchedContent(String[] argv, int length, String script, String alias) {
-        String urlAlias = argv[2];
-        String url = null;
-        if (YamlConfig.getProperty(INNER_URL, urlAlias) != null) {
-            url = YamlConfig.getProperty(INNER_URL, urlAlias);
-        }
-        else if (YamlConfig.getProperty(OUTER_URL, urlAlias) != null) {
-            Map<String, String> vpnMap = YamlConfig.getPropertiesMap(VPN);
-            List<String> vpnAlias = vpnMap.keySet().stream().limit(1).toList();
-            open(vpnAlias.get(0));
-            url = YamlConfig.getProperty(OUTER_URL, urlAlias);
-        }
-        else {
-            // 如果urlAlias既不在inner_url里也不在outer_url里，即不是网址别名
-            if (length == 3) {
-                url = URLUtil.isURL(urlAlias) ? urlAlias : getSearchUrlWithEngine(urlAlias);
-            }
-            else if (length == 4) {
-                // 如果指定了搜索引擎
-                String engine = argv[3];
-                url = getSearchUrlWithEngine(urlAlias, engine);
-            }
-        }
-        open(alias, url);
-    }
+			Process process = Runtime.getRuntime().exec(command);
+			process.waitFor();
 
-    private static String getSearchUrlWithEngine(String urlAlias) {
-        String engine = YamlConfig.getProperty(SETTING, SEARCH_ENGINE);
-        return getSearchUrlWithEngine(urlAlias, engine);
-    }
+			// 捕获输出
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				 BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))) {
 
-    private static String getSearchUrlWithEngine(String urlAlias, String engine) {
-        String searchPattern;
-        if (engine.equalsIgnoreCase(GOOGLE)) {
-            searchPattern = GOOGLE_SEARCH;
-        }
-        else if (engine.equalsIgnoreCase(BING)) {
-            searchPattern = BING_SEARCH;
-        }
-        else if (engine.equalsIgnoreCase(BAIDU)) {
-            searchPattern = BAIDU_SEARCH;
-        }
-        else {
-            LogUtil.info("未指定搜索引擎，使用默认搜索引擎：%s", BING);
-            searchPattern = BING_SEARCH;
-        }
-        return String.format(searchPattern, urlAlias);
-    }
+				String line;
+				while ((line = reader.readLine()) != null) {
+					LogUtil.info("脚本输出: %s", line);
+				}
+				while ((line = errorReader.readLine()) != null) {
+					LogUtil.error("脚本错误: %s", line);
+				}
+			}
 
-    public static boolean open(String alias, String filePath) {
-        try {
-            String path = YamlConfig.getProperty(PATH, alias);
-            if (path == null || path.trim().isEmpty()) {
-                LogUtil.error("❌ 未找到别名对应的路径: %s。请检查路径配置。", alias);
-                return false;
-            }
+			int exitCode = process.exitValue();
+			LogUtil.info("✅ 脚本执行完成，退出码: %d", exitCode);
+		} catch (IOException | InterruptedException e) {
+			LogUtil.error("💥 执行脚本失败: %s", e.getMessage());
+		}
+	}
 
-            // 根据操作系统类型选择命令
-            String command = "";
-            if (WINDOWS.equals(osType)) {
-                command = String.format("cmd /c start \"\" \"%s\" \"%s\"", path, filePath);
-                Runtime.getRuntime().exec(command);
-            }
-            else if (MAC.equals(osType)) {
-                String[] commands = {"open", path, filePath};
-                Runtime.getRuntime().exec(commands);
-            }
-            else {
-                LogUtil.error("💥 当前操作系统不支持此功能: %s", osType);
-                return false;
-            }
 
-            LogUtil.info("✅ 启动 {%s}，路径 %s: {%s}", alias, filePath, path);
-            return true;
-        }
-        catch (IOException e) {
-            LogUtil.error("💥 启动 %s 失败，文件 %s: %s", alias, filePath, e.getMessage());
-            e.printStackTrace();
-            return false;
-        }
-    }
+	private static void openVPN(String alias) {
+		open(alias);
+	}
 
-    public static boolean open(String alias) {
-        try {
-            String path = getPathByAlias(alias);
+	private static void openEditor(String[] argv, int length, String alias) {
+		if (length != 3) {
+			open(alias);
+		}
+		else {
+			String filePath = argv[2];
+			open(alias, filePath);
+		}
+	}
 
-            if (path == null || path.trim().isEmpty()) {
-                return false;
-            }
+	private static void openBrowser(String[] argv, int length, String alias, String script) {
+		if (length == 2) {
+			open(alias);
+		}
+		else {
+			openBrowserWithUrlAliasOrSearchedContent(argv, length, script, alias);
+		}
+	}
 
-            // 根据操作系统类型选择命令
-            if (WINDOWS.equals(osType)) {
-                String command = String.format("cmd /c start \"\" \"%s\"", path);
-                Runtime.getRuntime().exec(command);
-            }
-            else if (MAC.equals(osType)) {
-                String[] commands = {"open", path};
-                Runtime.getRuntime().exec(commands);
-            }
-            else {
-                LogUtil.error("💥 当前操作系统不支持此功能: %s", osType);
-                return false;
-            }
+	private static boolean isAliasExist(String alias) {
+		return YamlConfig.containProperty(PATH, alias)
+				|| YamlConfig.containProperty(INNER_URL, alias)
+				|| YamlConfig.containProperty(OUTER_URL, alias);
+	}
 
-            LogUtil.info("✅ 启动 %s : {%s}", alias, path);
-            return true;
-        }
-        catch (IOException e) {
-            LogUtil.error("💥 启动 %s 失败: %s", alias, e.getMessage());
-            e.printStackTrace();
-            return false;
-        }
-    }
+	private static void openBrowserWithUrlAliasOrSearchedContent(String[] argv, int length, String script, String alias) {
+		String urlAlias = argv[2];
+		String url = null;
+		if (YamlConfig.getProperty(INNER_URL, urlAlias) != null) {
+			url = YamlConfig.getProperty(INNER_URL, urlAlias);
+		}
+		else if (YamlConfig.getProperty(OUTER_URL, urlAlias) != null) {
+			Map<String, String> vpnMap = YamlConfig.getPropertiesMap(VPN);
+			List<String> vpnAlias = vpnMap.keySet().stream().limit(1).toList();
+			open(vpnAlias.get(0));
+			url = YamlConfig.getProperty(OUTER_URL, urlAlias);
+		}
+		else {
+			// 如果urlAlias既不在inner_url里也不在outer_url里，即不是网址别名
+			if (length == 3) {
+				url = URLUtil.isURL(urlAlias) ? urlAlias : getSearchUrlWithEngine(urlAlias);
+			}
+			else if (length == 4) {
+				// 如果指定了搜索引擎
+				String engine = argv[3];
+				url = getSearchUrlWithEngine(urlAlias, engine);
+			}
+		}
+		open(alias, url);
+	}
 
-    private static String getPathByAlias(String alias) {
-        String path = null;
-        if (YamlConfig.containProperty(PATH, alias)) {
-            path = YamlConfig.getProperty(PATH, alias);
-        }
-        else if (YamlConfig.containProperty(INNER_URL, alias)) {
-            path = YamlConfig.getProperty(INNER_URL, alias);
-        }
-        else if (YamlConfig.containProperty(OUTER_URL, alias)) {
-            path = YamlConfig.getProperty(OUTER_URL, alias);
-        }
-        else {
-            LogUtil.error("❌ 未找到别名对应的路径或网址: %s。请检查配置文件。", alias);
-        }
-        return path;
-    }
+	private static String getSearchUrlWithEngine(String urlAlias) {
+		String engine = YamlConfig.getProperty(SETTING, SEARCH_ENGINE);
+		return getSearchUrlWithEngine(urlAlias, engine);
+	}
 
-    public static String getOsType() {
-        return osType;
-    }
+	private static String getSearchUrlWithEngine(String urlAlias, String engine) {
+		String searchPattern;
+		if (engine.equalsIgnoreCase(GOOGLE)) {
+			searchPattern = GOOGLE_SEARCH;
+		}
+		else if (engine.equalsIgnoreCase(BING)) {
+			searchPattern = BING_SEARCH;
+		}
+		else if (engine.equalsIgnoreCase(BAIDU)) {
+			searchPattern = BAIDU_SEARCH;
+		}
+		else {
+			LogUtil.info("未指定搜索引擎，使用默认搜索引擎：%s", BING);
+			searchPattern = BING_SEARCH;
+		}
+		return String.format(searchPattern, urlAlias);
+	}
+
+	public static boolean open(String alias, String filePath) {
+		try {
+			String path = YamlConfig.getProperty(PATH, alias);
+			if (path == null || path.trim().isEmpty()) {
+				LogUtil.error("❌ 未找到别名对应的路径: %s。请检查路径配置。", alias);
+				return false;
+			}
+
+			// 根据操作系统类型选择命令
+			String command = "";
+			if (WINDOWS.equals(osType)) {
+				command = String.format("cmd /c start \"\" \"%s\" \"%s\"", path, filePath);
+				Runtime.getRuntime().exec(command);
+			}
+			else if (MAC.equals(osType)) {
+				String[] commands = {"open", path, filePath};
+				Runtime.getRuntime().exec(commands);
+			}
+			else {
+				LogUtil.error("💥 当前操作系统不支持此功能: %s", osType);
+				return false;
+			}
+
+			LogUtil.info("✅ 启动 {%s}，路径 %s: {%s}", alias, filePath, path);
+			return true;
+		} catch (IOException e) {
+			LogUtil.error("💥 启动 %s 失败，文件 %s: %s", alias, filePath, e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static boolean open(String alias) {
+		try {
+			String path = getPathByAlias(alias);
+
+			if (path == null || path.trim().isEmpty()) {
+				return false;
+			}
+
+			// 根据操作系统类型选择命令
+			if (WINDOWS.equals(osType)) {
+				String command = String.format("cmd /c start \"\" \"%s\"", path);
+				Runtime.getRuntime().exec(command);
+			}
+			else if (MAC.equals(osType)) {
+				String[] commands = {"open", path};
+				Runtime.getRuntime().exec(commands);
+			}
+			else {
+				LogUtil.error("💥 当前操作系统不支持此功能: %s", osType);
+				return false;
+			}
+
+			LogUtil.info("✅ 启动 %s : {%s}", alias, path);
+			return true;
+		} catch (IOException e) {
+			LogUtil.error("💥 启动 %s 失败: %s", alias, e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	private static String getPathByAlias(String alias) {
+		String path = null;
+		if (YamlConfig.containProperty(PATH, alias)) {
+			path = YamlConfig.getProperty(PATH, alias);
+		}
+		else if (YamlConfig.containProperty(INNER_URL, alias)) {
+			path = YamlConfig.getProperty(INNER_URL, alias);
+		}
+		else if (YamlConfig.containProperty(OUTER_URL, alias)) {
+			path = YamlConfig.getProperty(OUTER_URL, alias);
+		}
+		else {
+			LogUtil.error("❌ 未找到别名对应的路径或网址: %s。请检查配置文件。", alias);
+		}
+		return path;
+	}
+
+	public static String getOsType() {
+		return osType;
+	}
 }
