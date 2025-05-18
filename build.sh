8fccbafbe517aaf25be896558a1a791a2154e59a
lingojack <3065225677@qq.com>
2025-05-19 01:51:40 +0800

    add a build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+jpackage \
+  --name work-copilot \
+  --input target/ \
+  --main-jar work-copilot-java-7.0.jar \
+  --main-class com.lingoutil.workcopilot.WorkCopilotApplication \
+  --type dmg\
+  --app-version 7.0
