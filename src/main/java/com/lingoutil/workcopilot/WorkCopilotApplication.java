1997d671550ac0383ca5908d683d41b888e703bf
gougouwen <gougouwen@tencent.com>
2025-11-02 22:11:58 +0800

    禁用自动环境拓展，bash改为!前缀

--- src/main/java/com/lingoutil/workcopilot/WorkCopilotApplication.java
+++ src/main/java/com/lingoutil/workcopilot/WorkCopilotApplication.java
@@ -198,6 +198,7 @@ public class WorkCopilotApplication {
                     .terminal(terminal)
                     .history(new DefaultHistory())
                     .completer(completer)
+                    .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
                     .build();
 
             // 自动建议
@@ -214,7 +215,7 @@ public class WorkCopilotApplication {
                     LogUtil.log("input: %s", input);
 
                     // !开头表示执行bash命令
-                    if (input.startsWith("`")) {
+                    if (input.startsWith("!")) {
                         String bashCommand = input.substring(1);
                         bashCommand = " " + bashCommand;
                         // 这里 work dir 和当前一样, 命令是 stateless 的
